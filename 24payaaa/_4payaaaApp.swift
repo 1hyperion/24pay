bf7a89e11ee0cf50a5bd3d28e77ab460b0e17795
1hyperion <costinvlad11@gmail.com>
2026-06-02 13:44:18 +0300

    Initial commit

---
+++ 24payaaa/_4payaaaApp.swift
@@ -0,0 +1,17 @@
+//
+//  _4payaaaApp.swift
+//  24payaaa
+//
+//  Created by Omaewa on 02.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct _4payaaaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
